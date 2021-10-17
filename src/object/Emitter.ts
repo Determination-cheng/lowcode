2af0d6fd837ededc5251325d287993f2048fc7ea
Cheng <1003306162@qq.com>
2021-10-17 22:33:37 +0800

    A 添加状态机

---
+++ src/object/Emitter.ts
@@ -0,0 +1,39 @@
+import { Observable } from "rxjs"
+import {log} from "util";
+
+type ObserverFunction = (data: any) => void
+
+export class Emitter<Topic extends string | number> {
+    private observers: Map<Topic, ObserverFunction[]>
+    constructor() {
+        this.observers = new Map<Topic, ObserverFunction[]>()
+    }
+
+    private addObserverFunction(topic: Topic, fn: ObserverFunction) {
+        if (!this.observers.has(topic)) this.observers.set(topic, [])
+        this.observers.get(topic)!.push(fn)
+    }
+
+    on(topic: Topic | Topic[]): Observable<any> {
+        return new Observable<any>(observer => {
+            if (Array.isArray(topic)) {
+                topic.forEach(t => {
+                    this.addObserverFunction(t, data => observer.next(data))
+                })
+            } else {
+                this.addObserverFunction(topic, data => observer.next(data))
+            }
+        })
+    }
+
+    emit(topic: Topic, data?: any) {
+        this.observers.get(topic)?.forEach(fn => fn(data))
+    }
+}
+
+//* 测试
+enum Topics { A, B, C }
+const emitter = new Emitter<Topics>()
+
+emitter.on(Topics.A).subscribe(data => console.log('a triggered', data))
+emitter.emit(Topics.A, 'this is A data')
---
+++ src/object/StateMachine.ts
@@ -0,0 +1,44 @@
+import { Emitter } from "./Emitter"
+
+type StateTransferFunction = (...args: any[]) => void
+
+export class StateMachine<
+    S extends string | number,
+    A extends string | number,
+    Topic extends string | number
+> extends Emitter<Topic> {
+    private state: S
+    private transferTable: Map<S, Map<A, [StateTransferFunction, S]>>
+    constructor(initialState: S) {
+        super()
+        this.state = initialState
+        this.transferTable = new Map()
+    }
+
+    private addTransfer (from: S, to: S, action: A, fn: StateTransferFunction) {
+        if (!this.transferTable.has(from)) this.transferTable.set(from, new Map())
+        this.transferTable.get(from)!.set(action, [fn, to])
+    }
+
+    public register(from : S | S[], to: S, action: A, fn: StateTransferFunction) {
+        if (Array.isArray(from)) {
+            from.forEach(s => this.addTransfer(s, to, action, fn))
+        } else {
+            this.addTransfer(from, to, action, fn)
+        }
+    }
+
+    public dispatch(action: A, ...data: any[]) {
+        const adjTable = this.transferTable.get(this.state)
+        const transfer = adjTable?.get(action)
+        if (!transfer) return false
+
+        const [fn, nextS] = transfer
+        fn(...data)
+        this.state = nextS
+
+        // <auto>
+        while (this.dispatch('<auto>' as A, ...data));
+        return true
+    }
+}
